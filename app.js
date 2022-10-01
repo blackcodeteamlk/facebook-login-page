fa08493ea956395d4b3cf2003aeafce4c082eb67
blackcodeteamlk <109899254+blackcodeteamlk@users.noreply.github.com>
2022-10-01 16:13:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('SignUp').addEventListener('click',
+function(){
+    document.querySelector('.sing-up-contener').style.display = 'flex';
+});
+document.querySelector('.icon').addEventListener('click',
+function(){
+    document.querySelector('.sing-up-contener').style.display = 'none';
+});
